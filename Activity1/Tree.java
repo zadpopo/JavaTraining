ac8d872857d29f69fae78aad88ee63e42efa6c12
zadpopo <39912714+zadpopo@users.noreply.github.com>
2025-12-03 18:34:02 +0800

    Add files via upload

---
+++ Activity1/Tree.java
@@ -0,0 +1,10 @@
+package oop1;
+
+public class Tree {
+	double height = 0; 
+	
+	void tall() {
+		height = 123;
+		System.out.println("The Tree is :" + height +" cm");
+	}
+}
---
+++ Activity1/activity1.java
@@ -0,0 +1,20 @@
+package oop1;
+
+
+public class activity1 {
+
+    public static void main(String[] args) {
+        // Create objects using the constructor
+        Book   manga = new Book();
+        House  kubo  = new House();
+        Tree   olive = new Tree();
+
+		manga.speak();
+		manga.find();
+		kubo.locate();
+		olive.tall();
+}
+}
+
+
+ 
