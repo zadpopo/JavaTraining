9297bcc97fd470f14a8a8a1bbed01339b3a35b10
Herneal Zanjie J. Uy <hzjuy@headoffice.corp.bpi.com.ph>
2025-12-04 19:19:34 +0800

    Seatwork2

---
+++ Activity2/WhiteBoard.java
@@ -0,0 +1,12 @@
+package oop1;
+
+
+public class WhiteBoard {
+    int addend1 = 0;
+    int addend2 = 0;
+
+    public int addNumbers(int addend1, int addend2) {
+        int total = addend1 + addend2;
+        return total;
+    }
+}
