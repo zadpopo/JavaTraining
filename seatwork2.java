8fd80d384d6abb522d88a8f13d1303a18486e6c7
Herneal Zanjie J. Uy <hzjuy@headoffice.corp.bpi.com.ph>
2025-12-11 14:01:12 +0800

    M2_Activity2

---
+++ seatwork2.java
@@ -0,0 +1,12 @@
+package oop1;
+
+public class seatwork2 {
+
+    public static void main(String[] args) {
+        WhiteBoard wb = new WhiteBoard();
+        
+        int total = wb.addNumbers(5,3);
+        System.out.println("The total is: " + String.valueOf(total));
+	
+}
+}
