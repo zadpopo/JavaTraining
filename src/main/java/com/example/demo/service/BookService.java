4287cba961f06a1e5d3012b50401bf9df9e056d0
zadpopo <39912714+zadpopo@users.noreply.github.com>
2026-03-30 11:11:54 +0800

    Add files via upload

---
+++ src/main/java/com/example/demo/service/BookService.java
@@ -0,0 +1,21 @@
+
+package com.example.demo.service;
+
+import org.springframework.stereotype.Component;
+import jakarta.annotation.PostConstruct;
+
+@Component
+public class BookService {
+
+    private final LoggerService logger;
+
+    public BookService(LoggerService logger) {
+        this.logger = logger;
+    }
+    
+    @PostConstruct
+    public void init() {
+        logger.log("M8 Acitivty 2 Book Service initialized!");
+    }
+    
+}
